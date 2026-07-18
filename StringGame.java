40df4ef8cb61e8e924620d6a36cfa058bed93403
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-07-18 09:40:01 +0530

    StringGame778A CodeForces

---
+++ StringGame.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class StringGame{
+    static String t, p;
+    static int[] order;
+    static boolean check(int k) {
+        boolean[] removed = new boolean[t.length()];
+        for (int i = 0; i < k; i++) {
+            removed[order[i] - 1] = true;
+        }
+        int j = 0;
+        for (int i = 0; i < t.length() && j < p.length(); i++) {
+            if (removed[i]) continue;
+            if (t.charAt(i) == p.charAt(j)) {
+                j++;
+            }
+        }
+        return j == p.length();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        t = sc.next();
+        p = sc.next();
+        int n = t.length();
+        order = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            order[i] = sc.nextInt();
+        }
+
+        int lo = 0, hi = n - 1;
+        int ans = 0;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (check(mid)) {
+                ans = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
