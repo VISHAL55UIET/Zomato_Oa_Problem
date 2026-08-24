1015a27920fc326d4d1c65c75530c2785261316b
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-08-25 00:41:12 +0530

    InputChnage

--- GCD.java
+++ GCD.java
@@ -11,7 +11,6 @@ class GCD{
         for (int num : nums) {
             maxVal = Math.max(maxVal, num);
         }
-
         int[] divisorFreq = new int[maxVal + 1];
         for (int i = 0; i < n; i++) { // N*sqrt(maxVal)
             int num = nums[i]; // 36
@@ -24,24 +23,20 @@ class GCD{
                 }
             }
         }
-
         long[] pairsWithGcd = new long[maxVal + 1];
         for (int g = maxVal; g >= 1; g--) {
             long count = divisorFreq[g];
             // nC2
             pairsWithGcd[g] = count * (count - 1) / 2;
-
             // Correction time
             for (int mult = 2 * g; mult <= maxVal; mult += g) {
                 pairsWithGcd[g] -= pairsWithGcd[mult];
             }
         }
-
         long[] prefixCountGcd = new long[maxVal + 1];
         for (int g = 1; g <= maxVal; g++) {
             prefixCountGcd[g] = prefixCountGcd[g - 1] + pairsWithGcd[g];
         }
-
         int[] result = new int[queries.length];
         for (int q = 0; q < queries.length; q++) { // O(Q * log(maxVal))
             long idx = queries[q];
@@ -50,7 +45,6 @@ class GCD{
             int temp = 1;
             while (l <= r) {
                 int mid_gcd = l + (r - l) / 2;
-
                 if (prefixCountGcd[mid_gcd] > idx) {
                     temp = mid_gcd;
                     r = mid_gcd - 1;
@@ -58,7 +52,6 @@ class GCD{
                     l = mid_gcd + 1;
                 }
             }
-
             result[q] = temp;
         }
         return result;
